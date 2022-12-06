486755e0c1b5add596504817dcdce427ed70a9e4
Artemxxx2 <akolobnevstudio@gmail.com>
2022-12-06 20:37:38 +0200

    email integration

--- app/Http/Controllers/Admin/Users/UsersStoreController.php
+++ app/Http/Controllers/Admin/Users/UsersStoreController.php
@@ -3,15 +3,20 @@
 namespace App\Http\Controllers\Admin\Users;
 
 use App\Http\Requests\Admin\Users\StoreRequest;
+use App\Mail\User\PasswordMail;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 
 class UsersStoreController extends BaseController
 {
     public function __invoke(StoreRequest $request)
     {
         $data = $request->validated();
+        $password = $data['password'];
+        $name = $data['name'];
         $data['password'] = Hash::make($data['password']);
+        Mail::to($data['email'])->send( new PasswordMail($password, $name));
         $data['role'] = (int)$data['role'];
         User::firstOrCreate(['email' => $data['email']],$data);
         return redirect()->route('users.index');
